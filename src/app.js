53cdfe58530855d991ff77c927fedae0b28c6409
Roman Ožana <roman@ozana.cz>
2023-03-22 10:09:12 +0100

    Update format

--- src/app.js
+++ src/app.js
@@ -7,24 +7,24 @@ if (process.env.NODE_ENV === 'production') console.log('[DEBUG] app is running i
 
 
 const app = angular
-		.module('example', [])
-		.directive('myExampleDirective', ['$interval', ($interval) => {
-			return {
-				restrict: 'EA',
-				templateUrl: templateUrl,
-				link: (scope, element, attrs) => {
+	.module('example', [])
+	.directive('myExampleDirective', ['$interval', ($interval) => {
+		return {
+			restrict: 'EA',
+			templateUrl: templateUrl,
+			link: (scope, element, attrs) => {
 
-					// load moment.js
-					require.ensure([], function (require) {
-						let moment = require('moment');
+				// load moment.js
+				require.ensure([], function (require) {
+					let moment = require('moment');
 
-						console.log('[DEBUG] moment.js was loaded...');
+					console.log('[DEBUG] moment.js was loaded...');
 
-						$interval(() => {
-									scope.time = moment().format("HH:mm:ss");
-								}, 100
-						);
-					});
-				}
-			}
-		}]);
+					$interval(() => {
+							scope.time = moment().format('HH:mm:ss');
+						}, 100,
+					);
+				});
+			},
+		}
+	}]);
